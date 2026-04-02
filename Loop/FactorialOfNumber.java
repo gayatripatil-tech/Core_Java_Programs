6dce738624786e7c4411683ea2dedb54f0a6b0da
gayatripatil-tech <gayatripatil1022@gmail.com>
2026-04-02 21:54:22 +0530

    Add files via upload

---
+++ Loop/FactorialOfNumber.java
@@ -0,0 +1,13 @@
+public class FactorialOfNumber {
+
+    public static void main(String[]args){
+        int fact=1;
+      int n=5;
+        for( int i=1; i<=n; i++){
+
+            fact=fact*i;
+        }
+        System.out.println("the factorial of given number "+n+" is: "+ fact);
+    }
+    
+}
