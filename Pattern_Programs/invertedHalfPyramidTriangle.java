001acf47bcea2ef611dcf824eef8ed2ffa21c8dd
gayatripatil-tech <gayatripatil1022@gmail.com>
2026-04-03 23:17:24 +0530

    Update invertedHalfPyramidTriangle.java

--- Pattern_Programs/invertedHalfPyramidTriangle.java
+++ Pattern_Programs/invertedHalfPyramidTriangle.java
@@ -9,7 +9,7 @@ public class invertedHalfPyramidTriangle{
         for(int i=1;i<=n;i++){
             for(int j=1; j<=n;j++){
 
-                if(i==1 || j==1 || i<=n){
+                if(i+j<=n+1){
                     System.out.print("* ");
                     
                 }
